165233134a077eb325ee1822d8b32d8a73243f69
MishelMichev <163438689+MishelMichev@users.noreply.github.com>
2024-03-27 16:08:26 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,73 +1,80 @@
 ﻿using System;
 using System.Collections.Generic;
-namespace prototype
-{
-    public class Program
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sigleton
     {
-        public static void Main(string[] args)
+        public class Program
         {
-            ColorManager colormanager = new ColorManager();
-
-            // Initialize with standard colors
-            colormanager["white"] = new Color(255, 255, 255);
-            colormanager["orange"] = new Color(255, 102, 0);
-            colormanager["violet"] = new Color(255, 0, 255);
-
-            // User adds personalized colors
-            colormanager["peace"] = new Color(200, 200,200);
-            colormanager["confidence"] = new Color(230, 120, 55);
-            colormanager["balance"] = new Color(245, 134, 220);
-
-            // User clones selected colors
-            Color color1 = colormanager["peace"].Clone() as Color;
-            Color color2 = colormanager["confidence"].Clone() as Color;
-            Color color3 = colormanager["balance"].Clone() as Color;
-
-            // Wait for user
-            Console.ReadKey();
+            public static void Main(string[] args)
+            {
+                LoadBalancer b1 = LoadBalancer.GetLoadBalancer();
+                LoadBalancer b2 = LoadBalancer.GetLoadBalancer();
+                LoadBalancer b3 = LoadBalancer.GetLoadBalancer();
+                LoadBalancer b4 = LoadBalancer.GetLoadBalancer();
+               
+                if (b1 == b2 && b2 == b3 && b3 == b4)
+                {
+                    Console.WriteLine("Same instance\n");
+                }
+               
+                LoadBalancer balancer = LoadBalancer.GetLoadBalancer();
+                for (int i = 0; i < 12; i++)
+                {
+                    string server = balancer.Server;
+                    Console.WriteLine("Dispatch Request to: " + server);
+                }
+                
+                Console.ReadKey();
+            }
         }
-    }
-  
-    public abstract class ColorPrototype
-    {
-        public abstract ColorPrototype Clone();
-    }
-    
-    public class Color : ColorPrototype
-    {
-        int white;
-        int orange;
-        int violet;
-       
-
-        public Color(int white, int orange, int violet)
-        {
-            this.white = white;
-            this.orange = orange;
-            this.violet = violet;
-        }
-
         
-        public override ColorPrototype Clone()
+        public class LoadBalancer
         {
-            Console.WriteLine(
-                "Cloning color RGB: {0,3},{1,3},{2,3}",
-                white, orange, violet);
-            return this.MemberwiseClone() as ColorPrototype;
-        }
-    }
+            static LoadBalancer instance;
+            List<string> servers = new List<string>();
+            Random random = new Random();
+            
+            private static object locker = new object();
     
-
-    public class ColorManager
-    {
-        private Dictionary<string, ColorPrototype> colors =
-            new Dictionary<string, ColorPrototype>();
-        // Indexer
-        public ColorPrototype this[string key]
-        {
-            get { return colors[key]; }
-            set { colors.Add(key, value); }
+            protected LoadBalancer()
+            {
+                
+                servers.Add("ServerI");
+                servers.Add("ServerII");
+                servers.Add("ServerIII");
+                servers.Add("ServerIV");
+                servers.Add("ServerV");
+                servers.Add("ServerVI");
+                servers.Add("ServerVII");
+                servers.Add("ServerIX");
+        }
+            public static LoadBalancer GetLoadBalancer()
+            {
+                
+                if (instance == null)
+                {
+                    lock (locker)
+                    {
+                        if (instance == null)
+                        {
+                            instance = new LoadBalancer();
+                        }
+                    }
+                }
+                return instance;
+            }
+           
+            public string Server
+            {
+                get
+                {
+                    int r = random.Next(servers.Count);
+                    return servers[r].ToString();
+                }
+            }
         }
     }
-}
 
